e9692f0b969145fe15cb7386743462072cc9bd30
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-11-02 17:32:37 +0200

    feat(App): add mock component
    check working store

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import React from 'react';
+import useAppStore from '@/store/store';
 import './App.css';
 
 function App() {
-	return <div className='bg-slate-200'>Hello!</div>;
+	const posts = useAppStore((state) => state.posts);
+	const addNewPost = useAppStore((state) => state.add);
+
+	console.log('🚀 ~ file: App.tsx ~ line 7 ~ posts', posts);
+	return (
+		<div className='bg-slate-200'>
+			{posts.map((post) => post.name)}
+			<button
+				className='p-1 bg-red-100 mx-3 '
+				onClick={() =>
+					addNewPost({ name: 'New', avatarUrl: 'sdf', message: 'dd' })
+				}>
+				Add
+			</button>
+		</div>
+	);
 }
 
 export default App;
