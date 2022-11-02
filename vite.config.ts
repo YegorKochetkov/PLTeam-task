9a2bcc77f762e856a9115aea5a77d912a01f5c42
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-11-02 17:31:23 +0200

    config(app): add dependencies, alias
    remove redundant file

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,4 +20,9 @@ export default defineConfig({
 			},
 		}),
 	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 });
