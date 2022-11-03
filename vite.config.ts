99a15f58b1a7737a3b914d1c2718a624ec0d0a4e
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-11-04 01:29:26 +0200

    config(app): remove @ alias
    doesn't work as it should: paths recognize, but suggestion doesn't work

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,7 +22,7 @@ export default defineConfig({
 	],
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, './src'),
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
 	},
 });
